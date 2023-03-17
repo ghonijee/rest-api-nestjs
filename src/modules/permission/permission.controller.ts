836c92a1319ac975e29fd887c864a84701e81942
ghonijee <akughoni@gmail.com>
2023-03-18 06:10:11 +0700

    feat(permission): get list permission with paginate
    - Add API response docs
    - add paginate filter data

--- src/modules/permission/permission.controller.ts
+++ src/modules/permission/permission.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Query } from '@nestjs/common';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { Permission } from '@prisma/client';
+import { PaginateFilterDTO } from 'src/common/dto/paginate-filterdto';
+import { PaginateResult } from 'src/common/resource/paginate.response';
 import { PermissionService } from './permission.service';
 
+@ApiTags('permission')
 @Controller('permission')
 export class PermissionController {
   constructor(private service: PermissionService) {}
+
   @Get('/')
-  paginate() {
-    return this.service.findPaginate();
+  @ApiCreatedResponse({ type: PaginateResult<Permission> })
+  @HttpCode(HttpStatus.OK)
+  paginate(@Query() data: PaginateFilterDTO) {
+    try {
+      return this.service.findPaginate(data);
+    } catch (error) {
+      throw error;
+    }
   }
 }
--- src/modules/permission/permission.service.ts
+++ src/modules/permission/permission.service.ts
@@ -1,11 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { Permission } from '@prisma/client';
+import { PaginateFilterDTO } from 'src/common/dto/paginate-filterdto';
+import {
+  PaginateMetaResult,
+  PaginateResult,
+} from 'src/common/resource/paginate.response';
 import { PrismaService } from 'src/common/services/prisma.service';
 
 @Injectable()
 export class PermissionService {
   constructor(private prisma: PrismaService) {}
 
-  findPaginate() {
-    return this.prisma.permission.findMany();
+  async findPaginate(
+    data: PaginateFilterDTO,
+  ): Promise<PaginateResult<Permission>> {
+    const [result, totalCount] = await this.prisma.$transaction([
+      this.prisma.permission.findMany({
+        skip: (data.page - 1) * data.limit,
+        take: data.limit,
+        orderBy: {
+          [data.orderBy]: data.order,
+        },
+        where: {
+          name: { contains: data.filter },
+        },
+      }),
+      this.prisma.permission.count({
+        where: {
+          name: { contains: data.filter },
+        },
+      }),
+    ]);
+    return new PaginateResult(
+      result,
+      new PaginateMetaResult({ requestData: data, totalCount: totalCount }),
+    );
   }
 }
