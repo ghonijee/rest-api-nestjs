d3cee3719e03ddcf86dfb4fb2a629844ca4b5201
ghonijee <akughoni@gmail.com>
2023-03-18 06:08:17 +0700

    feat(common):  pagination data
    - create paginate DTO
    - create paginate response

---
+++ src/common/dto/paginate-filterdto.ts
@@ -0,0 +1,40 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsInt, IsOptional, IsString } from 'class-validator';
+import { toNumber } from '../helpers/cast.helper';
+
+export class PaginateFilterDTO {
+  @ApiProperty({ default: 1 })
+  @Transform(({ value }) => toNumber(value), {
+    toClassOnly: true,
+  })
+  @IsInt()
+  page: number;
+
+  @ApiProperty({ default: 10 })
+  @Transform(({ value }) => toNumber(value), {
+    toClassOnly: true,
+  })
+  @IsInt()
+  limit: number;
+
+  @ApiProperty({ default: 0 })
+  @Transform(({ value }) => toNumber(value))
+  @IsInt()
+  skip: number;
+
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional()
+  filter?: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional()
+  orderBy?: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional()
+  order?: string;
+}
---
+++ src/common/resource/paginate.response.ts
@@ -0,0 +1,56 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { PaginateFilterDTO } from '../dto/paginate-filterdto';
+
+interface PaginateFilterData {
+  requestData: PaginateFilterDTO;
+
+  totalCount: number;
+}
+
+export class PaginateMetaResult {
+  @ApiProperty()
+  readonly page: number;
+
+  @ApiProperty()
+  readonly limit: number;
+
+  @ApiProperty()
+  readonly itemCount: number;
+
+  @ApiProperty()
+  readonly pageCount: number;
+
+  @ApiProperty()
+  readonly previous: number;
+
+  @ApiProperty()
+  readonly next: number;
+
+  @ApiProperty()
+  readonly hasPreviousPage: boolean;
+
+  @ApiProperty()
+  readonly hasNextPage: boolean;
+
+  constructor({ requestData, totalCount }: PaginateFilterData) {
+    this.page = requestData.page;
+    this.limit = requestData.limit;
+    this.itemCount = totalCount;
+    this.pageCount = Math.ceil(this.itemCount / this.limit);
+    this.hasPreviousPage = this.page > 1;
+    this.previous = this.page > 1 ? this.page - 1 : null;
+    this.hasNextPage = this.page < this.pageCount;
+    this.next = this.page < this.pageCount ? this.page + 1 : null;
+  }
+}
+
+export class PaginateResult<T> {
+  data: T[];
+
+  meta: PaginateMetaResult;
+
+  constructor(data: T[], meta: PaginateMetaResult) {
+    this.data = data;
+    this.meta = meta;
+  }
+}
