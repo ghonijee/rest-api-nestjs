fe10a32ca5cc51f4973fc561c0fc5943ca2dd6f1
ghonijee <akughoni@gmail.com>
2023-03-29 16:00:47 +0700

    feat(common): add model serialization on paginate response

--- src/common/decorator/api-response-paginate.decorator.ts
+++ src/common/decorator/api-response-paginate.decorator.ts
@@ -2,13 +2,22 @@ import { applyDecorators, UseInterceptors, SetMetadata } from '@nestjs/common';
 import {
   RESPONSE_MESSAGE,
   RESPONSE_OK_STATUS,
+  RESPONSE_SERIALIZATION,
 } from '../constant/response.constant';
 import { ApiResponsePaginateInterceptor } from '../interceptor/api-response-paginate.interceptor';
+import { IResponseOptions } from '../interfaces/response.interface';
 
-export function ApiResponsePaginate<T>(message: string): MethodDecorator {
+export function ApiResponsePaginate<T>(
+  message: string,
+  options?: IResponseOptions<T>,
+): MethodDecorator {
   return applyDecorators(
     UseInterceptors(ApiResponsePaginateInterceptor<T>),
     SetMetadata(RESPONSE_MESSAGE, message),
     SetMetadata(RESPONSE_OK_STATUS, true),
+    SetMetadata(
+      RESPONSE_SERIALIZATION,
+      options == null ? null : options.serialization,
+    ),
   );
 }
